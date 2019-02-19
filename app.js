8e77fae822066614b24e6339213225a466dfebc5
Ka Wong <kawong2020@u.northwestern.edu>
2019-02-18 20:31:35 -0600

    initial sockets snippet

--- app.js
+++ app.js
@@ -17,92 +17,27 @@ app.use('/', routes);
 app.use('/secure', passport.authenticate('jwt', { session : false }), secureRoute );
 
 
-app.listen(8000, function () {
+const server = app.listen(8000, function () {
   console.log("listening on port 8000");
 })
 
-/* Simple get function from database. Does not actually get anything from database */
+const io = require('socket.io')(server);
+let history = [];
 
-/* Local strategy. How to implement with API implementation? */
-// /* Using passport.js, check if entered username and password match with database */
-// passport.use(new LocalStrategy({
-//   usernameField: 'email',
-//   passwordField: 'password'
-// },
-//   function (email, password, done) {
-//     User.findOne({
-//       where: { email: email }
-//     }).then(user => {
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect email address.' });
-//       }
-//       bcrypt.compare(password, user.password)
-//         .then(result => {
-//           if (result) {
-//             return done(null, user);
-//           } else {
-//             return done(null, false, { message: 'Incorrect password.' });
-//           }
-//         }).catch(err => {
-//           return done(null, false, { message: 'Runtime issues.' });
-//         })
-//     })
-//   }
-// ));
+class Message {
+    constructor(name, msg) {
+        this.sender = name;
+        this.message = msg;
+    }
+}
 
-//This verifies that the token sent by the user is valid --scotch.io
-// passport.use(new JWTstrategy({
-//   //secret we used to sign our JWT
-//   secretOrKey: 'minion',
-//   //we expect the user to send the token as a query paramater with the name 'secret_token'
-//   jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
-// }, async (token, done) => {
-//   try {
-//     //Pass the user details to the next middleware
-//     return done(null, token.user);
-//   } catch (error) {
-//     done(error);
-//   }
-// }));
-
-/* Issue tokens--based on separate tutorial */
-// passport.use(
-//   'jwt',
-//   new JWTstrategy(opts, (jwt_payload, done) => {
-//     try {
-//       User.findOne({
-//         where: {
-//           username: jwt_payload.id,
-//         },
-//       }).then(user => {
-//         if (user) {
-//           console.log('user found in db in passport');
-//           done(null, user);
-//         } else {
-//           console.log('user not found in db');
-//           done(null,false);
-//         }
-//       });
-//     } catch (err) {
-//       done(err);
-//     }
-//   }),
-// );
-
-// /* Login with local strategy */
-// app.post('/login', passport.authenticate('local', { failureRedirect: '/error' }),
-//   function (req, res) {
-
-//     res.json('Success!');
-
-//     app.get('/profile', (req, res, next) => {
-
-//     })
-
-//     app.get('/authenticated', function (req, res) {
-//       res.json(req.user);
-//     })
-
-//     app.get('/error', function (req, res) {
-//       res.json('Error.');
-//     }));
+io.on('connection', function (socket) {
+    socket.emit('send', history);
+    socket.on('message', function(data) {
+        console.log(data);
+        let obj = new Message(data.name,data.msg);
+        history.push(obj);
+        socket.emit('send', [obj]);
+        socket.broadcast.emit('send', [obj]);
+    })
+});
